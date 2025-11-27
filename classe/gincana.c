4c4808f523ddedb5a49f05a15908ab5f235925bf
LeandroFalcao19 <leandrodiaspaesfalcao11@gmail.com>
2025-11-27 16:46:02 -0300

    Alocação da qtd
    coloquei as quantidade de cartelas compradas para cada candidato e separei um grupo para meninos e um pra meninas competirem entre si

---
+++ classe/gincana.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+
+int main(){
+    
+    int opt;
+    int Joao = 0, Pedro = 0, Leandro = 0;
+    int Maria = 0, Julia = 0, Bruna = 0;
+    int grupo1[] = {Joao, Pedro, Leandro};
+    int grupo2[] = {Maria, Julia, Bruna};
+    int qtd;
+    int candidato;
+
+    while (1)
+    {
+        printf("\nMENU\n");
+        printf("[1] Explicacao da gincana\n");
+        printf("[2] Comprar cartela \n");
+        printf("[3] Ver ganhadores\n");
+        printf("[0] Sair \n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opt);
+
+        if(opt == 0) {
+            printf("Saindo...\n");
+            break;
+        }
+
+        switch (opt) {
+
+            case 1:
+            printf("\n\n EXPLICACAO DA GINCANA \n\n");
+            printf("E ai, pessoal! Preparem os chapeus e os vestidos de chita! \n");
+            printf("A nossa tradicional Gincana Junina de Xerife e Sinhazinha esta de volta e a competicao vai ser quente!\n");
+            printf("Este ano, a disputa pelo titulo mais cobicado da festa envolve nao so carisma, mas tambem muita estrategia!\n\n");
+
+            printf("Como funciona? E simples e divertido!\n");
+            printf("Os candidatos a Xerife e Sinhazinha estao com suas cartelas de voto a postos.\n");
+            printf("Cada cartela vendida conta como um ponto para o seu candidato favorito.\n");
+            printf("O objetivo e mobilizar a galera, vender o maximo de cartelas possivel e garantir a vitoria!\n\n");
+
+            printf("- Cada cartela = um voto!\n");
+            printf("- O candidato (ou dupla) que acumular a maior quantidade de cartelas vendidas ate o dia da apuracao\n");
+            printf("  sera coroado o GRANDE VENCEDOR da nossa festa junina! \n\n");
+            break;
+
+            case 2:
+
+             printf("\n\n COMPRAR CARTELA \n\n");
+             printf("Escolha o candidato:\n");
+             printf("[1] Joao\n");
+             printf("[2] Pedro\n");
+             printf("[3] Leandro\n");
+             printf("[4] Maria\n");
+             printf("[5] Julia\n");
+             printf("[6] Bruna\n");
+             printf("Escolha um candidato: ");
+             scanf("%d", &candidato);
+             
+             printf("Quantas cartelas voce deseja comprar? ");
+             scanf("%d", &qtd);
+
+             if(qtd > 0){
+              printf("Voce comprou %d cartela(s) com sucesso!\n", qtd);
+
+             switch (candidato){
+             case 1: Joao += qtd; break;
+             case 2: Pedro += qtd; break;
+             case 3: Leandro += qtd; break;
+             case 4: Maria += qtd; break;
+             case 5: Julia += qtd; break;
+             case 6: Bruna += qtd; break;
+             default: printf("Candidato invalido!\n");
+            }
+            }
+            break;
+
+            case 3:
+
+    }
+}
+return 0; 
+}
